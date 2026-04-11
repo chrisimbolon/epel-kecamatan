d0f6eb218b7fd975a76be829510c4dc933e4a246
Christyan Simbolon <sambungdata@gmail.com>
2026-04-12 02:37:00 +0700

    feat-backend: add pegawai AppConfig

---
+++ backend/modules/epel/pegawai/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class PegawaiConfig(AppConfig):
+    name = "modules.epel.pegawai"
+    label = "pegawai"
+    verbose_name = "Data Pegawai"
+    default_auto_field = "django.db.models.BigAutoField"
